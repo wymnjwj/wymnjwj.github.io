3cdd731f39e633ff635535f95653822336cdad62
wymnjwj <34032537+wymnjwj@users.noreply.github.com>
2025-02-17 17:35:50 +0800

    github not support active page

---
+++ script/all_post.js
@@ -0,0 +1,22 @@
+document.write("<aside>")
+document.write("  <ul class=\"text-margin-tl\"><a href=\"#\" >git操作</a></ul>");
+document.write("  <ul class=\"text-margin-l\"><a href=\"#\" >我的测试0</a></ul>");
+document.write("  <ul class=\"text-margin-l\"><a href=\"#\" >我的测试1</a></ul>");
+document.write("  <ul class=\"text-margin-bl\"><a href=\"#\" >我的测试2</a></ul>");
+document.write("</aside>")
+document.write("<article class=\"blog-post\">                    ");
+document.write("    <h2>git操作</h2>                 ");
+document.write("    <p class=\"post-date\">2025年2月6日</p>  ");
+document.write("</article>                                     ");
+document.write("<article class=\"blog-post\">                    ");
+document.write("    <h2>我的测试0</h2>                 ");
+document.write("    <p class=\"post-date\">2025年2月7日</p>  ");
+document.write("</article>                                     ");
+document.write("<article class=\"blog-post\">                    ");
+document.write("    <h2>我的测试1</h2>                 ");
+document.write("    <p class=\"post-date\">2025年2月7日</p>  ");
+document.write("</article>                                     ");
+document.write("<article class=\"blog-post\">                    ");
+document.write("    <h2>我的测试2</h2>                 ");
+document.write("    <p class=\"post-date\">2025年2月7日</p>  ");
+document.write("</article>                                     ");
---
+++ script/parse_post.py
@@ -0,0 +1,58 @@
+#! /usr/bin/python3
+import re
+import os
+
+def parse_all_post(path, o_f):
+  paths=os.scandir(path)
+  o_file = open(o_f,'w')
+  post_dict={}
+  for file in paths:
+      if (file.is_file()) and (re.match("\d{1,4}\_\d{1,2}\_\d{1,2}\_\d{1,2}\_\d{1,2}\_\d{1,3}\_(.*)\.md",file.name)):
+         tmp_file = str(file.name)
+         tmp_str  = tmp_file.split("_")
+         tmp_year   = tmp_str[0].ljust(4,'0')
+         tmp_mounth = tmp_str[1].ljust(2,'0')
+         tmp_day    = tmp_str[2].ljust(2,'0')
+         tmp_hour   = tmp_str[3].ljust(2,'0')
+         tmp_minites= tmp_str[4].ljust(2,'0')
+         tmp_id     = tmp_str[5].ljust(3,'0')
+         tmp_post_time = tmp_year+tmp_mounth+tmp_day+tmp_hour+tmp_minites+tmp_id
+         tmp_post_tile =  tmp_str[6][:-3]
+         post_dict[tmp_post_time]=tmp_post_tile
+      print(file.name)
+  
+  sorted_post_dict = dict(sorted(post_dict.items()))
+
+  post_list=[]
+  i=0 
+
+  for tmp_post_time in sorted_post_dict:
+     tmp_list = [tmp_post_time,sorted_post_dict[tmp_post_time]]
+     post_list.append(tmp_list)
+     i=i+1
+
+  for ii in range(i):
+    if(ii==0):
+      o_file.writelines("document.write(\"<aside>\")\n")
+      o_file.writelines("document.write(\"  <ul class=\\\"text-margin-tl\\\"><a href=\\\"#\\\" >"+ post_list[ii][1]+"</a></ul>\");\n")
+    elif (ii==i-1):
+      o_file.writelines("document.write(\"  <ul class=\\\"text-margin-bl\\\"><a href=\\\"#\\\" >"+post_list[ii][1]+"</a></ul>\");\n")
+      o_file.writelines("document.write(\"</aside>\")\n")
+    else:
+      o_file.writelines("document.write(\"  <ul class=\\\"text-margin-l\\\"><a href=\\\"#\\\" >"+post_list[ii][1]+"</a></ul>\");\n")
+
+  for ii in range(i):
+      o_file.writelines("document.write(\"<article class=\\\"blog-post\\\">                    \");\n")
+      o_file.writelines("document.write(\"    <h2>"+ post_list[ii][1]+"</h2>                 \");\n")
+      o_file.writelines("document.write(\"    <p class=\\\"post-date\\\">"+ post_list[ii][0][0:4]+"年"+post_list[ii][0][5:6]+"月"+post_list[ii][0][7:8]+"日"+"</p>  \");\n")
+      o_file.writelines("document.write(\"</article>                                     \");\n")
+    
+
+  o_file.close()
+
+def main():
+  parse_all_post("../doc","all_post.js")
+
+
+if __name__ == '__main__':
+  main()
